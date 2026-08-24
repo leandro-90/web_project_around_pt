d45950e492749683c40e1d54e548e9557cdf3ae9
Leandro <ferreir720@gmail.com>
2026-08-24 04:10:33 -0300

    New upgrade of project

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,65 @@ const initialCards = [
   },
 ];
 
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+// Função para fechar o modal
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  const profileTitle = document.querySelector(".profile__title");
+  const profileDescription = document.querySelector(".profile__description");
+  const nameInput = document.querySelector(".popup__input_type_name");
+  const descriptionInput = document.querySelector(
+    ".popup__input_type_description",
+  );
+
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  const editPopup = document.querySelector("#edit-popup");
+  fillProfileForm();
+  openModal(editPopup);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  const nameInput = document.querySelector(".popup__input_type_name");
+  const descriptionInput = document.querySelector(
+    ".popup__input_type_description",
+  );
+
+  const profileTitle = document.querySelector(".profile__title");
+  const profileDescription = document.querySelector(".profile__description");
+
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+
+  const editPopup = document.querySelector("#edit-popup");
+  closeModal(editPopup);
+}
+
+const editProfileButton = document.querySelector(".profile__edit-button");
+editProfileButton.addEventListener("click", handleOpenEditModal);
+
+const editPopupCloseButton = document.querySelector(
+  "#edit-popup .popup__close",
+);
+editPopupCloseButton.addEventListener("click", () => {
+  const editPopup = document.querySelector("#edit-popup");
+  closeModal(editPopup);
+});
+
+const editProfileForm = document.querySelector("#edit-profile-form");
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
+
 initialCards.forEach((card) => {
   console.log(card.name);
 });
